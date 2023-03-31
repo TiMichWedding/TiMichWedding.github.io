22262e5ac09a13381d1a5429025cd9660280db9b
TiMichWedding <129243486+TiMichWedding@users.noreply.github.com>
2023-03-31 11:41:02 -0700

    Add files via upload

--- static/main-1.js
+++ static/main-1.js
@@ -63,7 +63,7 @@ $(function(){
 			e.preventDefault();
 			$('#festivities').children().not('iframe, .icn-history-close').fadeIn();
 		});
-
+	
 	$('#languageSelector').show();
 	$('#languageSelector a')
 		.on('click', function(e){
@@ -79,6 +79,26 @@ $(function(){
 				}, 1000);
 			}
 		});
+
+	(function($) {
+		$.QueryString = (function(a) {
+			if (a == "") return {};
+			var b = {};
+			for (var i = 0; i < a.length; ++i)
+			{
+				var p=a[i].split('=');
+				if (p.length != 2) continue;
+				b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+			}
+			return b;
+		})(window.location.search.substr(1).split('&'))
+	})(jQuery);
+	 
+	// Usage
+	var param = jQuery.QueryString["lg"];
+	if(param === "chinese") {
+		$('#languageSelector a[toggle=chinese]').click();
+	}
 
 	$("img[data-original]").lazyload();
 });
